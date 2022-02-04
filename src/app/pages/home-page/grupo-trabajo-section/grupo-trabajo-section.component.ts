d50b255259dfcd5c35fc5700f40ff6986a3b4111
AlmaDividida <david_626180497@hotmail.com>
2022-02-03 21:57:31 -0600

    Se agregaron los componentes del home-page

---
+++ src/app/pages/home-page/grupo-trabajo-section/grupo-trabajo-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grupo-trabajo-section',
+  templateUrl: './grupo-trabajo-section.component.html',
+  styleUrls: ['./grupo-trabajo-section.component.css']
+})
+export class GrupoTrabajoSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
